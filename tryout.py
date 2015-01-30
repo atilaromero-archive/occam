13c477e934a5540821ce6d27bf93be1e856299b2
user <user@localhost.localdomain>
2015-01-29 22:23:13 -0200

    incomplete ideas on choices and predictions

--- tryout.py
+++ tryout.py
@@ -3,10 +3,33 @@
 import random
 from equilibrium import A, B, C, D, E, F, piece
 
-def rand(moves,obj,goal):
+def rand(moves):
     """the most basic and memoryless strategy"""
-    while obj.value != goal:
-        print random.choice(moves)()
+    return random.choice(moves)
+
+memory={}
+
+def linear(moves):
+    """assume moves produce a linear effect on obj"""
+    return  random.choice(moves)
+
+def choosemove(moves):
+    for move in moves:
+        if memory.has_key(move):
+        
+def makepredictions(memory):
+    
 
 def test():
-    rand([A,B,C,D,E,F],piece,0)
+    while obj.value != goal:
+        strategy = None
+        while strategy == None:
+            strategy = rand([rand,linear])
+        state = obj.value
+        move = strategy([A,B,C,D,E,F])
+        result = move()
+        if not memory.has_key(move):
+            memory[move]=[]
+        memory[move].append((state,move,result))
+
+test()
